622a600ee6cb2c3d361a781e64fb0151364e217c
jonasnikitha <jonasnikitha@gmail.com>
2025-06-30 00:01:07 +0200

    Added my first Customer DATA i my database

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE alx_book_store;
 
-INSERT TABLE Customers(customer_id, customer_name, email, address) 
+INSERT TABLE customer(customer_id, customer_name, email, address) 
 VALUES(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
